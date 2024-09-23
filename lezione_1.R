5e26ac3abdf4f339cbc8a98d320497c9fd4f6e9f
Vincenzo Nardelli <vincnardelli@gmail.com>
2024-09-23 14:27:05 +0200

    Add files via upload

---
+++ lezione_1.R
@@ -0,0 +1,117 @@
+3 + 5
+12/7
+
+result <- 3 + 5
+result = 3 + 5
+
+x <- result * 3.1415
+
+x
+print(x)
+
+nome_molto_lungo
+nome.molto.lungo
+
+
+vettore <- c(1, 3, 8, 13)
+vettore
+
+
+vettore[1]
+vettore[4]
+
+vettore[1:3]
+vettore[c(1, 2, 3)]
+
+vettore[c(1, 3)]
+
+vettore > 3
+vettore[vettore > 3]
+
+vettore == 3
+vettore[vettore == 3]
+
+vettore != 3
+vettore[vettore != 3]
+
+# commento
+stringa <- "questo è un testo"
+
+
+
+numero1 <- 3
+numero2 <- "3"
+
+numero1 == numero2
+
+class(numero1)
+class(numero2)
+class(stringa)
+a <- c(1, 2, 5)
+c(3, a)
+
+sum(a)
+mean(a)
+min(a)
+max(a)
+
+
+## Esempio con nomi e numeri preferiti
+
+nomi <- c("a", "b", "c", "d")
+numeri <- c(3, 6, 8, 9)
+
+nomi[numeri > 8]
+
+
+# Agg mese di nascita
+## Data Frame
+
+data <- data.frame(nomi = nomi, 
+           numeri = numeri)
+
+data
+
+class(data)
+str(data)
+dim(data)
+
+View(data)
+
+data$nomi
+data$numeri
+
+
+
+# Caricare dati
+
+# Da pacchetto R
+data("mtcars")
+data("USPersonalExpenditure")
+?USPersonalExpenditure
+
+# Da un nuovo pacchetto R
+library(ISLR)
+data("Hitters")
+str(Hitters)
+
+data("Smarket")
+summary(Smarket)
+str(Smarket)
+
+# Carico dati da Excel
+library(readxl)
+esercizio_1 <- read_excel("esercizio_1.xlsx")
+View(esercizio_1)
+
+
+esercizio_1 <- read.csv("esercizio_1.csv")
+
+
+# calcoliamo la media
+voti <- esercizio_1
+(voti$Economia_Aziendale + voti$Matematica_Generale + voti$Informatica)/3
+
+# calcoliamo la media ponderata?
+
+
